bd1953d5f0431aed0237b3976b64deb9a6f05770
castillorosy <rosycastillo18@yahoo.com>
2015-05-26 14:03:06 -0700

    trying to make title screen appear

--- screens/title.js
+++ screens/title.js
@@ -3,8 +3,9 @@ game.TitleScreen = me.ScreenObject.extend({
      *  action to perform on state change
      **/
     onResetEvent: function() {
-        var titleImage = new me.sprite(0, 0, me.loader.getImage("title-screen"));
-        me.game.world.addChild(titleImage, 1);
+        me.game.world.addChild(new me.sprite(0, 0, me.loader.getImage('title-screen')),
+        me.input.bindKey(me.input.KEY.ENTER, "start");
+//        me.game.world.addChild(titleImage, 1)
     },
     /**	
      *  action to perform when leaving this screen (state change)

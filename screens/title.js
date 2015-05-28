7a1bf6850d8e2069d53152df86f01254e028b553
castillorosy <rosycastillo18@yahoo.com>
2015-05-28 13:59:16 -0700

    ew work

--- screens/title.js
+++ screens/title.js
@@ -5,6 +5,16 @@ game.TitleScreen = me.ScreenObject.extend({
     onResetEvent: function() {
         me.game.world.addChild(new me.sprite(0, 0, me.loader.getImage('title-screen')),
                 me.input.bindKey(me.input.KEY.ENTER, "start"),
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function() {
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.Font("Arial", 46, "white");
+                    },
+                    draw: function(renderer) {
+                        this.font.draw(renderer.getContext(), "Jurassic Park", 540, 130);
+                          this.font.draw(renderer.getContext(), "Press Enter TO Play", 250, 530);
+                    }
+                }))),
                 this.handler = me.event.subcribe(me.event.KEYDOWN, function(action, keyCode, edge) {
                     if (action === "start") {
                         me.state.change(me.state.PLAY);
@@ -13,6 +23,6 @@ game.TitleScreen = me.ScreenObject.extend({
                 ));
     },
     onDestoryEvent: function() {
-        
+        me.input.unbindKey(me.input.KEY.ENTER);
     }
 });
